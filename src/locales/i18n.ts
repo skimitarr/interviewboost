70a5007617d1e7d362437818b53a6208ee7d93ed
Konstantin <konstantinz@wizardsdev.com>
2023-10-21 10:44:35 +0300

    add ru locale

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,24 +1,28 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import en from './en.json';
+import ru from './ru.json';
 
 i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: en,
-      },
-    },
-    lng: 'en',
-    nsSeparator: false,
-    load: 'currentOnly',
-    react: {
-      useSuspense: true,
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translation: en,
+            },
+            ru: {
+                translation: ru,
+            },
+        },
+        lng: 'ru',
+        nsSeparator: false,
+        load: 'currentOnly',
+        react: {
+            useSuspense: true,
+        },
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
 export default i18n;
