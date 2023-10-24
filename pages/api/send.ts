76db6738f1e06b7f1e753361f39fa3fa5717867b
unknown <grisha.lakoza@gmail.com>
2023-10-24 17:58:53 +0300

    locales2

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,25 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../src/app/components/EmailTemplate';
+// import { EmailTemplate } from '../../src/app/components/EmailTemplate';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 // const RESEND_API_KEY = 're_GrzT9RGi_3RM6RM5iRR8BNGz9vUn6SFGx';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    // @ts-ignore
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['interview.questions50@gmail.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+  // try {
+  //   // @ts-ignore
+  //   const data = await resend.emails.send({
+  //     from: 'Acme <onboarding@resend.dev>',
+  //     to: ['interview.questions50@gmail.com'],
+  //     subject: 'Hello world',
+  //     react: EmailTemplate({ firstName: 'John' }),
+  //   });
 
-    res.status(200).json(data);
-    console.log(data)
-  } catch (error) {
-    res.status(400).json(error);
-  }
+  //   res.status(200).json(data);
+  //   console.log(data)
+  // } catch (error) {
+  //   res.status(400).json(error);
+  // }
 };
 
 
