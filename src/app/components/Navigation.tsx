a2cee43162f08e66ad31cf6e39737bb8b6873eb4
unknown <grisha.lakoza@gmail.com>
2023-11-03 21:55:42 +0200

    btn logout

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -2,34 +2,55 @@
 import { useSession, signOut } from "next-auth/react"
 import Link from "next/link"
 import { usePathname } from 'next/navigation'
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useTranslation } from 'react-i18next'
-import classnames from "classnames"
 import { Props } from "./Types"
+import classnames from "classnames"
 
-const Navigation = ({ navLinks }: Props) => {
+export function Navigation({ navLinks }: Props) {
   const [wantSignOut, setWantSignOut] = useState(false);
   const { t } = useTranslation();
   const pathname = usePathname()
   const session = useSession();
+  const signoutLinkRef = useRef<HTMLAnchorElement | null>(null)
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (signoutLinkRef.current && !signoutLinkRef.current.contains(event.target as Node)) {
+        setWantSignOut(false);
+      }
+    }
+
+    if (wantSignOut) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [wantSignOut]);
 
   return (
     <header className="header">
       <div className="header__wrapper-1">
-        {
-          navLinks.map(link => {
-            const isActive = pathname === link.href
-            return <Link key={link.label} href={link.href} className={classnames('header__link', { 'active': isActive })}>
-              <p className="header__link-text">{t(link.label)}</p>
-            </Link>
-          })}
+        {navLinks.map(link => {
+          return <Link key={link.label} href={link.href}
+            className={classnames(
+              'header__link',
+              { 'active': pathname === link.href }
+            )}>
+            <p className="header__link-text">{t(link.label)}</p>
+          </Link>
+        })}
       </div>
       <div className="header__wrapper-2">
         {session?.data
           ? <>
             <p className="header__email">{session.data.user?.email}</p>
             <img onClick={() => setWantSignOut(!wantSignOut)} src="../../../signIn.svg" alt="profile's logo" />
-            {wantSignOut && <Link className="header__link header__link-signout btn" href='#' onClick={() => signOut({ callbackUrl: '/' })}>{t('logOut')}</Link>}
+            {wantSignOut && <Link className="header__link header__link-signout btn" ref={signoutLinkRef} href='#' onClick={() => signOut({ callbackUrl: '/' })}>{t('logOut')}</Link>}
           </>
           : <Link className="header__link header__link-signin" href='/signin'>
             <p className="header__link-text btn">{t('signIn')}</p>
@@ -39,4 +60,3 @@ const Navigation = ({ navLinks }: Props) => {
     </header>
   )
 }
-export { Navigation }
